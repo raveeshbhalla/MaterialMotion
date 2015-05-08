c8c9fc12364f4517a89538f7cbcb86758d252279
Raveesh Bhalla <raveeshbhalla90@gmail.com>
2015-05-09 02:21:30 +0530

    Added onScrollListener

--- parallax/src/main/java/in/raveesh/materialmotion/ParallaxActivity.java
+++ parallax/src/main/java/in/raveesh/materialmotion/ParallaxActivity.java
@@ -22,12 +22,12 @@ public class ParallaxActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parallax);
 
-        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 
         toolbar.setTitleTextColor(getResources().getColor(R.color.primary_text_default_material_dark));
         setSupportActionBar(toolbar);
 
-        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
 
         MovieAdapter adapter = new MovieAdapter(Resources.getMovies(this), new OnMovieClickListener() {
@@ -38,11 +38,23 @@ public class ParallaxActivity extends ActionBarActivity {
         });
         recyclerView.setAdapter(adapter);
 
-        ImageView hero = (ImageView)findViewById(R.id.hero);
+        ImageView hero = (ImageView) findViewById(R.id.hero);
         hero.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 ImageActivity.launch(ParallaxActivity.this, new Movie("Guardians of the Galaxy", getResources().getDrawable(R.drawable.guardians)));
+            }
+        });
+
+        recyclerView.setOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
             }
         });
     }
