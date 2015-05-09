5144587e41e355a612c8149ccd5faabe612d588e
Raveesh Bhalla <raveeshbhalla90@gmail.com>
2015-05-09 06:39:49 +0530

    Combined scroll with toolbar alpha

--- parallax/src/main/java/in/raveesh/materialmotion/ParallaxActivity.java
+++ parallax/src/main/java/in/raveesh/materialmotion/ParallaxActivity.java
@@ -1,11 +1,13 @@
 package in.raveesh.materialmotion;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import in.raveesh.materialmotion.parallax.R;
@@ -17,18 +19,38 @@ import in.raveesh.materialmotion.resources.Resources;
 
 public class ParallaxActivity extends ActionBarActivity {
 
+    RecyclerView recyclerView;
+    Toolbar toolbar;
+    ImageView hero;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parallax);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitleTextColor(getResources().getColor(R.color.primary_text_default_material_dark));
         setSupportActionBar(toolbar);
 
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+
+        hero = (ImageView) findViewById(R.id.hero);
+        hero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageActivity.launch(ParallaxActivity.this, new Movie("Guardians of the Galaxy", getResources().getDrawable(R.drawable.guardians)));
+            }
+        });
+
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+
+        final Drawable toolbarBg = toolbar.getBackground();
+        toolbarBg.setAlpha(0);
 
         MovieAdapter adapter = new MovieAdapter(Resources.getMovies(this), new OnMovieClickListener() {
             @Override
@@ -38,15 +60,12 @@ public class ParallaxActivity extends ActionBarActivity {
         });
         recyclerView.setAdapter(adapter);
 
-        ImageView hero = (ImageView) findViewById(R.id.hero);
-        hero.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ImageActivity.launch(ParallaxActivity.this, new Movie("Guardians of the Galaxy", getResources().getDrawable(R.drawable.guardians)));
-            }
-        });
+        ViewGroup.LayoutParams params = hero.getLayoutParams();
+        final int heroHeight = params.height;
 
         recyclerView.setOnScrollListener(new RecyclerView.OnScrollListener() {
+            int totalScroll = 0;
+
             @Override
             public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
                 super.onScrollStateChanged(recyclerView, newState);
@@ -55,6 +74,18 @@ public class ParallaxActivity extends ActionBarActivity {
             @Override
             public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
                 super.onScrolled(recyclerView, dx, dy);
+
+                totalScroll += dy;
+
+                int alpha = totalScroll/heroHeight * 255;
+                if (alpha > 255){
+                    alpha = 255;
+                }
+                else if (alpha < 0){
+                    alpha = 0;
+                }
+
+                toolbarBg.setAlpha(alpha);
             }
         });
     }
